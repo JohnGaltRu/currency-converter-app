974f6f5dacd78f7194e590815a2baf874f5cc2d0
JohnGaltRu <ntts2@list.ru>
2019-05-31 15:00:00 +0300

    Updates

---
+++ precache-manifest.d418cf25e520b32e37d30276df018cda.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4c79def5d0c6a6583b6bd422ef84ed6",
+    "url": "/currency-converter-app/index.html"
+  },
+  {
+    "revision": "22cc12b3338ebddd6013",
+    "url": "/currency-converter-app/static/css/main.c98396bf.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/currency-converter-app/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "22cc12b3338ebddd6013",
+    "url": "/currency-converter-app/static/js/main.a26deae1.chunk.js"
+  },
+  {
+    "revision": "718bf1a361b9f3c248d0",
+    "url": "/currency-converter-app/static/js/runtime~main.de6384ee.js"
+  }
+]);
